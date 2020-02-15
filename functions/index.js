226e6ebc17c8a4f59ef71532e6bc4c674ff38a29
Tor Gilbertson <tor@wisetail.com>
2020-02-15 12:50:30 -0700

    rm subscription

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,6 @@ const functions = require('firebase-functions')
 admin.initializeApp(functions.config().firebase)
 
 const postLikes = require('./lib/postLikes')
-const subscriptions = require('./lib/subscriptions')
-
-exports.updateStripeSubscription = functions
-  .firestore
-  .document('subscriptions/{subscriptionId}')
-  .onWrite(subscriptions.updateStripeSubscription)
 
 exports.updatePostLikeCount = functions
   .firestore
