c4d5e15eb9ea5a0158d3b8b6256d70c6e57ca71d
Tor Gilbertson <tor@wisetail.com>
2020-02-16 01:06:01 -0700

    add presence

--- src/actions/logIn.js
+++ src/actions/logIn.js
@@ -1,16 +1,19 @@
-import Firebase from 'firebase/app'
+import Firebase from 'firebase/app';
+
+import { setPresence } from './presence';
 
 const logIn = () => {
+  const provider = new Firebase.auth.GithubAuthProvider();
 
-  const provider = new Firebase.auth.GithubAuthProvider()
-  
   return Firebase.auth()
     .signInWithRedirect(provider)
-    .then( result => {
-      console.log(`logged in as ${result.user.displayName}`)
-    }).catch( error => {
-      console.error('could not sign in', error)
+    .then(result => {
+      setPresence();
+      console.log(`logged in as ${result.user.displayName}`);
     })
-}
+    .catch(error => {
+      console.error('could not sign in', error);
+    });
+};
 
-export default logIn
+export default logIn;
