271c23323666286f0e4bef291259d71a8f3662e0
Tor Gilbertson <tor@wisetail.com>
2020-02-16 21:52:17 -0700

    form, editor css rework

--- src/styles/editor.js
+++ src/styles/editor.js
@@ -1,7 +1,9 @@
+import { Colors } from '@wisetail/tokens';
+
 export default {
   minHeight: '20em',
   borderRadius: '3px',
-  border: '1px solid #ddd',
+  border: `1px solid ${Colors.SILVER_20}`,
   fontFamily: '"Fira code", monospace',
   fontSize: '1.6rem'
 }
