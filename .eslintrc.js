934ff14a2059c1b22d1b7baec65898252e626ab5
Tor Gilbertson <tor@wisetail.com>
2020-02-16 22:06:25 -0700

    lint fixes for fs helpers

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
+    'no-underscore-dangle': 'off',
     'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
   },
   settings: {
