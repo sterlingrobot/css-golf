5b9d31485b01b9aaa772ccc73412d511b89584fc
Tor Gilbertson <tor@wisetail.com>
2020-02-18 22:21:50 -0700

    move admin check to firebase function

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,10 @@ const functions = require('firebase-functions');
 
 admin.initializeApp(functions.config().firebase);
 
+const getUserRole = require('./lib/getUserRole');
 const challengeLikes = require('./lib/challengeLikes');
+
+exports.setAccessPrivileges = functions.auth.onCreate(getUserRole.isAdmin);
 
 exports.updatePostLikeCount = functions.firestore
   .document('challengeLikes/{challengeLikeId}')
