a837ef605aaeda1049c55a8e2d63c2b8dad4f1c5
Tor Gilbertson <tor@wisetail.com>
2020-02-23 08:47:26 -0700

    catch scss compile errors

--- src/actions/createAttempt.js
+++ src/actions/createAttempt.js
@@ -7,16 +7,19 @@ const createAttempt = async (challengeId, values) => {
   const _likeCount = 0;
 
   const attempt = { ...values, challenge: challengeId, _likeCount };
-  attempt.style = await compileScss('attempt', challengeId, attempt.css);
 
-  return Firebase.firestore()
+  const doc = await Firebase.firestore()
     .collection('attempts')
     .add(prepareDocForCreate(attempt))
-    .then(data => ({ path: data.path, ...attempt }))
     .catch(error => {
       // eslint-disable-next-line no-alert
       alert(`Whoops, couldn't save your attempt: ${error.message}`);
     });
+
+  return compileScss('attempt', doc.id, attempt.css)
+    .then(styles => doc.update({ style: styles }))
+    .then(() => Promise.resolve({ path: doc.path, ...attempt }))
+    .catch(error => Promise.reject(error));
 };
 
 export default createAttempt;
