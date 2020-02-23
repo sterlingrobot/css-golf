7ab3ca3d09f8203d44442480a2290d1e50147053
Tor Gilbertson <tor@wisetail.com>
2020-02-23 16:30:33 -0700

    style for challenge/attempt compare

--- src/views/attempts/Attempt.js
+++ src/views/attempts/Attempt.js
@@ -6,59 +6,96 @@ import { Route } from 'react-router-dom';
 
 import Error from '../misc/Error';
 
-import { Page } from '../../styles/layout';
+import AttemptForm from './AttemptForm';
 import ChallengeOutput from '../challenges/ChallengeOutput';
 import AttemptOutput from './AttemptOutput';
 
-const Attempt = ({ match }) => (
-  <Page>
-    <FirestoreDocument path={`attempts/${match.params.id}`}>
-      {({ error, isLoading, data }) => {
-        if (error) {
-          return <Error error={error} />;
-        }
+import createAttempt from '../../actions/createAttempt';
 
-        if (isLoading) {
-          return <p>loading...</p>;
-        }
+import { Page } from '../../styles/layout';
 
-        if (data.length === 0) {
-          return <Error />;
-        }
+import '../../styles/attempt.scss';
 
-        const attempt = data;
+class Attempt extends React.Component {
+  constructor() {
+    super();
+    this.resetError = this.resetError.bind(this);
+  }
 
-        return (
-          <FirestoreDocument path={`challenges/${attempt.challenge}`}>
-            {({ error, isLoading, data }) => {
-              if (error) {
-                return <Error error={error} />;
-              }
+  state = {
+    error: null
+  };
 
-              if (isLoading) {
-                return <p>loading...</p>;
-              }
+  resetError(_e) {
+    this.setState({ error: null });
+  }
 
-              if (data.length === 0) {
-                return <Error />;
-              }
+  render() {
+    const { match } = this.props;
+    return (
+      <Page>
+        <FirestoreDocument path={`attempts/${match.params.id}`}>
+          {({ error, isLoading, data }) => {
+            if (error) {
+              return <Error error={error} />;
+            }
 
-              const challenge = data;
+            if (isLoading) {
+              return <p>loading...</p>;
+            }
 
-              return (
-                <div>
-                  <h2>{challenge.title}</h2>
-                  <ChallengeOutput challenge={challenge} />
-                  <AttemptOutput attempt={attempt} challenge={challenge} />
-                </div>
-              );
-            }}
-          </FirestoreDocument>
-        );
-      }}
-    </FirestoreDocument>
-  </Page>
-);
+            if (data.length === 0) {
+              return <Error />;
+            }
+
+            const attempt = data;
+
+            return (
+              <FirestoreDocument path={`challenges/${attempt.challenge}`}>
+                {({ error, isLoading, data }) => {
+                  if (error) {
+                    return <Error error={error} />;
+                  }
+
+                  if (isLoading) {
+                    return <p>loading...</p>;
+                  }
+
+                  if (data.length === 0) {
+                    return <Error />;
+                  }
+
+                  const challenge = data;
+
+                  return (
+                    <div className="attempt-container">
+                      <h2>{challenge.title}</h2>
+                      <ChallengeOutput challenge={challenge} />
+                      <AttemptOutput attempt={attempt} challenge={challenge} />
+                      <AttemptForm
+                        attempt={attempt}
+                        challenge={challenge}
+                        path={attempt.path}
+                        error={this.state.error}
+                        onSubmit={values =>
+                          createAttempt(
+                            challenge.id,
+                            values
+                          ).catch(({ error }) => this.setState({ error }))
+                        }
+                        onClick={this.resetError}
+                      />
+                    </div>
+                  );
+                }}
+              </FirestoreDocument>
+            );
+          }}
+        </FirestoreDocument>
+      </Page>
+    );
+  }
+}
 
 export default Attempt;
 
