a5a9cb223e74f6421e62e792a5534128db639b70
Tor Gilbertson <tor@wisetail.com>
2020-02-23 11:38:01 -0700

    set updatedOn for update

--- src/actions/createAttempt.js
+++ src/actions/createAttempt.js
@@ -1,7 +1,10 @@
 import Firebase from 'firebase/app';
 
 import { compileScss } from './compileScss';
-import { prepareDocForCreate } from './helpers/firestoreHelpers';
+import {
+  prepareDocForCreate,
+  prepareDocForUpdate
+} from './helpers/firestoreHelpers';
 
 const createAttempt = async (challengeId, values) => {
   const _likeCount = 0;
@@ -12,14 +15,14 @@ const createAttempt = async (challengeId, values) => {
     ? Firebase.firestore().doc(attempt.path)
     : await Firebase.firestore()
         .collection('attempts')
-        .add(prepareDocForCreate({ ...attempt }))
+        .add(prepareDocForCreate(attempt))
         .catch(error => {
           // eslint-disable-next-line no-alert
           alert(`Whoops, couldn't save your attempt: ${error.message}`);
         });
 
   if (attempt.path) {
-    await doc.update({ ...attempt });
+    await doc.update(prepareDocForUpdate(attempt));
   }
 
   return compileScss('attempt', doc.id, attempt.css)
