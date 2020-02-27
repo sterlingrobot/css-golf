de85475fd08240b73e7a08f8fb54873e054e8c43
Tor Gilbertson <tor@wisetail.com>
2020-02-27 08:47:08 -0700

    set initial canvas height to 0

--- src/views/attempts/DiffOutput.js
+++ src/views/attempts/DiffOutput.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // eslint-disable-next-line react/display-name
 const DiffOutput = React.forwardRef((_props, ref) => (
-  <canvas className="diff-output" ref={ref}></canvas>
+  <canvas className="diff-output" ref={ref} width="100%" height="0"></canvas>
 ));
 
 export default DiffOutput;
