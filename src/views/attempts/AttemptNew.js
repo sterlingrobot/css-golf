ffe65b0c6fa500577b22f6631e7c4a6e123e23d3
Tor Gilbertson <tor@wisetail.com>
2020-02-26 22:55:29 -0700

    fix redirect path for edit

--- src/views/attempts/AttemptNew.js
+++ src/views/attempts/AttemptNew.js
@@ -9,7 +9,7 @@ import Error from '../misc/Error';
 import AttemptForm from './AttemptForm';
 import ChallengeOutput from '../challenges/ChallengeOutput';
 
-import createAttempt from '../../actions/createAttempt';
+import saveAttempt from '../../actions/saveAttempt';
 
 import { Page } from '../../styles/layout';
 
@@ -85,8 +85,8 @@ class AttemptNew extends React.Component {
                         path={this.state.path}
                         error={this.state.error}
                         onSubmit={values =>
-                          createAttempt(challenge.id, values)
-                            .then(attempt => history.push(attempt.path))
+                          saveAttempt(challenge.id, values)
+                            .then(attempt => history.push(`/${attempt.path}`))
                             // if we error on SCSS compile, we've still
                             //  created the doc, so store that in state
                             //  to pass back into the form values
