7ad66cfacaca30ededa1bf72a3edd52ffba7e409
Tor Gilbertson <tor@wisetail.com>
2020-02-27 23:56:17 -0700

    rm snapshot from attempt-output

--- src/views/attempts/AttemptOutput.js
+++ src/views/attempts/AttemptOutput.js
@@ -2,30 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import '../../styles/challenge.scss';
-import { OutputImg } from '../../styles/challenge';
 
 const setContent = html => {
   return { __html: html };
 };
 
 // eslint-disable-next-line react/display-name
-const AttemptOutput = React.forwardRef(
-  ({ attempt, challenge, isCompare }, ref) => (
-    <div id={`attempt-${attempt.id}`} className="attempt-output" ref={ref}>
-      {isCompare ? (
-        <OutputImg src={attempt.snapshot} />
-      ) : (
-        <>
-          <style>{attempt.style}</style>
-          <div
-            className="attempt-content"
-            dangerouslySetInnerHTML={setContent(challenge.html)}
-          ></div>
-        </>
-      )}
-    </div>
-  )
-);
+const AttemptOutput = React.forwardRef(({ attempt, html }, ref) => (
+  <div id={`attempt-${attempt.id}`} className="attempt-output" ref={ref}>
+    <style>{attempt.style}</style>
+    <div
+      className="attempt-content"
+      dangerouslySetInnerHTML={setContent(html)}
+    ></div>
+  </div>
+));
 
 export default AttemptOutput;
 
@@ -35,8 +26,5 @@ AttemptOutput.propTypes = {
     style: PropTypes.string,
     snapshot: PropTypes.string
   }),
-  challenge: PropTypes.shape({
-    html: PropTypes.string
-  }),
-  isCompare: PropTypes.bool
+  html: PropTypes.string
 };
