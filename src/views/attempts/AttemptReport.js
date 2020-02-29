45a3e7a2f7d5d627a86619559568f9c2dbbeeebb
Tor Gilbertson <tor@wisetail.com>
2020-02-29 14:48:20 -0700

    add efficiency, weighted scoring

--- src/views/attempts/AttemptReport.js
+++ src/views/attempts/AttemptReport.js
@@ -2,20 +2,80 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { ReportTable } from '../../styles/report';
+import '../../styles/report.scss';
 
-const AttemptReport = ({ title, diff, lint }) => (
-  <wds-panel title={title}>
+const WEIGHTS = {
+  diff: 6,
+  lint: 3,
+  efficiency: 1
+};
+
+const weightedAmount = name => {
+  const weightTotal = Object.values(WEIGHTS).reduce(
+    (acc, curr) => acc + curr,
+    0
+  );
+  return (WEIGHTS[name] / weightTotal) * 100;
+};
+
+const scoreDiff = diff => ({
+  name: 'diff',
+  score: diff && 100 - (diff.diffPixels / diff.totalPixels) * 100
+});
+
+const scoreLint = lint => ({
+  name: 'lint',
+  score: lint && 100 - 10 * lint.warnings.length
+});
+
+const scoreEfficiency = efficiency => ({
+  name: 'efficiency',
+  score:
+    efficiency &&
+    100 -
+      ((efficiency.match.length - efficiency.target.length) /
+        efficiency.target.length) *
+        100
+});
+
+const scoreTotal = (diff, lint, efficiency) => {
+  const scores = [
+    scoreDiff(diff),
+    scoreLint(lint),
+    scoreEfficiency(efficiency)
+  ].filter(num => !!num.score);
+  let average = 0;
+  return (
+    scores.reduce((acc, curr) => {
+      const weight = WEIGHTS[curr.name];
+      average += weight;
+      acc += curr.score * weight;
+      return acc;
+    }, 0) / average
+  );
+};
+
+const AttemptReport = ({ title, diff, lint, efficiency, children }) => (
+  <wds-panel className="attempt-report" title={title}>
+    <div slot="header">{children}</div>
     <ReportTable>
       <thead>
         <tr>
-          <th colSpan="2">&nbsp;</th>
-          <th className="score">Score</th>
+          <th className="score" colSpan="2">
+            Score
+          </th>
+          <th className="score">
+            {scoreTotal(diff, lint, efficiency).toFixed(2)}
+          </th>
         </tr>
       </thead>
       <tbody>
         {diff && (
           <tr>
-            <th>Pixel Diffing Result</th>
+            <th>
+              Pixel Diffing Result
+              <small>Worth {weightedAmount('diff').toFixed(2)}%</small>
+            </th>
             <td>
               <ul>
                 <li>
@@ -24,14 +84,15 @@ const AttemptReport = ({ title, diff, lint }) => (
                 </li>
               </ul>
             </td>
-            <td className="score">
-              {(100 - (diff.diffPixels / diff.totalPixels) * 100).toFixed(2)}
-            </td>
+            <td className="score">{scoreDiff(diff).score.toFixed(2)}</td>
           </tr>
         )}
         {lint && (
           <tr>
-            <th>Linting Result</th>
+            <th>
+              Linting Result
+              <small>Worth {weightedAmount('lint').toFixed(2)}%</small>
+            </th>
             <td>
               <ul>
                 {lint.warnings.length ? (
@@ -45,8 +106,30 @@ const AttemptReport = ({ title, diff, lint }) => (
                 )}
               </ul>
             </td>
+            <td className="score">{scoreLint(lint).score.toFixed(2)}</td>
+          </tr>
+        )}
+        {efficiency && (
+          <tr>
+            <th>
+              Efficiency Result
+              <small>Worth {weightedAmount('efficiency').toFixed(2)}%</small>
+            </th>
+            <td>
+              <ul>
+                <li>
+                  {(() => {
+                    const charDiff =
+                      efficiency.match.length - efficiency.target.length;
+                    return `${Math.abs(charDiff)} characters ${
+                      charDiff >= 0 ? 'more' : 'less'
+                    } than challenge`;
+                  })()}
+                </li>
+              </ul>
+            </td>
             <td className="score">
-              {(100 - 10 * lint.warnings.length).toFixed(2)}
+              {scoreEfficiency(efficiency).score.toFixed(2)}
             </td>
           </tr>
         )}
@@ -72,5 +155,10 @@ AttemptReport.propTypes = {
         text: PropTypes.string
       })
     )
-  })
+  }),
+  efficiency: PropTypes.shape({
+    target: PropTypes.string,
+    match: PropTypes.string
+  }),
+  children: PropTypes.node
 };
