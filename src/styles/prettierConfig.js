14054a9c80ac9ee0e60e7fad5da4e38885688c8f
Tor Gilbertson <tor@wisetail.com>
2020-03-01 22:52:37 -0700

    fix prettier config export

--- src/styles/prettierConfig.js
+++ src/styles/prettierConfig.js
@@ -1,16 +1,13 @@
-export default options => {
-  return {
-    parser: 'js',
-    useTabs: false,
-    tabWidth: 2,
-    endOfLine: 'lf',
-    printWidth: 80,
-    semi: true,
-    singleQuote: true,
-    bracketSpacing: true,
-    trailingComma: 'all',
-    jsxBracketSameLine: false,
-    arrowParens: 'avoid',
-    ...options
-  };
+export default {
+  parser: 'js',
+  useTabs: false,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  trailingComma: 'all',
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid'
 };
