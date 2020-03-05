5af3d03e24d74f239ed1c2908094f163710d745a
Tor Gilbertson <tor@wisetail.com>
2020-03-04 23:39:58 -0700

    require deps in separate file

--- functions/generate-styles/generate-styles.js
+++ functions/generate-styles/generate-styles.js
@@ -6,6 +6,9 @@ module.exports.handler = async (event, _context) => {
     const styles = sass
       .renderSync({
         data: `
+          @import 'variables';
+          @import 'mixins';
+
           #${prefix}-${id} { ${css} }
         `,
         includePaths: [
---
+++ functions/generate-styles/index.js
@@ -0,0 +1,7 @@
+const material = require('@material/elevation');
+const tokens = require('@wisetail/tokens');
+
+module.exports = {
+  material,
+  tokens
+};
