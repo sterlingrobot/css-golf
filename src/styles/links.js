8c79a8cce430f9f7168ca0fd3cd8250f0a9f31e3
Tor Gilbertson <tor@wisetail.com>
2020-03-07 08:59:35 -0700

    use a href for error link to refresh the app

--- src/styles/links.js
+++ src/styles/links.js
@@ -3,35 +3,42 @@ import { Link } from 'react-router-dom';
 
 import { Colors } from '@wisetail/tokens';
 
-const InternalLink = styled(Link)`
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: flex-start;
-  align-items: center;
-  margin: 1em 1em 0 0;
-  color: ${Colors.BLUE_BASE};
+const linkStyle = `
+display: flex;
+flex-flow: row nowrap;
+justify-content: flex-start;
+align-items: center;
+margin: 1em 1em 0 0;
+color: ${Colors.BLUE_BASE};
+text-decoration: none;
+
+&:hover,
+&:active {
   text-decoration: none;
+}
 
-  &:hover,
-  &:active {
-    text-decoration: none;
-  }
+> wds-icon {
+  margin-right: 0.25em;
+  color: inherit;
+  transform: translate(0, 0.025em);
+  transition: 250ms transform;
+}
 
-  > wds-icon {
-    margin-right: 0.25em;
-    color: inherit;
-    transform: translate(0, 0.025em);
-    transition: 250ms transform;
-  }
-
-  &:hover > wds-icon {
-    transform: translate(-0.2em, 0.025em);
-  }
+&:hover > wds-icon {
+  transform: translate(-0.2em, 0.025em);
+}
 `;
+const InternalLink = styled(Link)`
+  ${linkStyle}
+`;
+const ExternalLink = styled.a`
+  ${linkStyle}
+`;
+
 const HeaderLink = styled(Link)`
   color: ${Colors.BLACK_BASE};
   text-decoration: none;
   font-size: 1.2rem;
 `;
 
-export { InternalLink, HeaderLink };
+export { InternalLink, ExternalLink, HeaderLink };
