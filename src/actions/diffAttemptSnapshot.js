d59a04b8f5ee08daa5f0320b8d89026dbf98abcf
Tor Gilbertson <tor@wisetail.com>
2020-03-07 08:24:27 -0700

    move diffing to action

---
+++ src/actions/diffAttemptSnapshot.js
@@ -0,0 +1,59 @@
+import pixelmatch from 'pixelmatch';
+import generateDiffData from '../../actions/generateDiffData';
+
+const DIFF_OPTIONS = {
+  threshold: 0.1,
+  diffMask: true
+};
+
+const getTotalNonAlphaPixels = imgData => {
+  const all = imgData.length;
+  let total = 0;
+  for (let i = 0; i < all; i += 4) {
+    if (imgData[i + 3] > 0) {
+      total++;
+    }
+  }
+  return total;
+};
+
+const diffPixels = ({ target, match, width, height, options }) => {
+  const imgDataOutput = new ImageData(width, height);
+  let totalPixels = getTotalNonAlphaPixels(target);
+  const diffPixels = pixelmatch(
+    target,
+    match,
+    imgDataOutput.data,
+    width,
+    height,
+    {
+      ...DIFF_OPTIONS,
+      ...options
+    }
+  );
+  return Promise.resolve({ totalPixels, diffPixels }, imgDataOutput);
+};
+
+const writeDiffToSnapshot = imgDataOutput => {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  canvas.width = imgDataOutput.width;
+  canvas.height = imgDataOutput.height;
+
+  ctx.putImageData(imgDataOutput, 0, 0);
+
+  return canvas.toDataURL();
+};
+
+const diffAttemptSnapshot = (target, match, options) =>
+  generateDiffData(target, match)
+    .then(imgData => diffPixels({ ...imgData, ...{ options } }))
+    .then((diff, imgDataOutput) =>
+      Promise.resolve({
+        diff,
+        snapshot: writeDiffToSnapshot(imgDataOutput)
+      })
+    );
+
+export default diffAttemptSnapshot;
--- src/views/attempts/DiffOutput.js
+++ src/views/attempts/DiffOutput.js
@@ -1,94 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import pixelmatch from 'pixelmatch';
-import generateDiffData from '../../actions/generateDiffData';
+import { OutputImg } from '../../styles/output';
 
-const DIFF_OPTIONS = {
-  threshold: 0.1,
-  diffMask: true
-};
+const DiffOutput = ({ snapshot }) => (
+  <div className="diff-output">
+    <OutputImg src={snapshot} />;
+  </div>
+);
 
-class DiffOutput extends React.Component {
-  constructor() {
-    super();
-    this.canvas = React.createRef();
-    this.diffPixels.bind(this);
-    this.writeDiffToOutput.bind(this);
-  }
-
-  getTotalNonAlphaPixels(imgData) {
-    const all = imgData.length;
-    let total = 0;
-    for (let i = 0; i < all; i += 4) {
-      if (imgData[i + 3] > 0) {
-        total++;
-      }
-    }
-    return total;
-  }
-
-  diffPixels({ target, match, width, height, options }) {
-    const imgDataOutput = new ImageData(width, height);
-    let totalPixels = this.getTotalNonAlphaPixels(target);
-    const diff = pixelmatch(target, match, imgDataOutput.data, width, height, {
-      ...DIFF_OPTIONS,
-      ...options
-    });
-    this.writeDiffToOutput(imgDataOutput);
-    return (
-      this.props.onDiffResult && this.props.onDiffResult(totalPixels, diff)
-    );
-  }
-
-  writeDiffToOutput(imgDataOutput) {
-    const canvas = this.canvas.current;
-    canvas.width = imgDataOutput.width;
-    canvas.height = imgDataOutput.height;
-    var ctx = canvas.getContext('2d');
-    ctx.putImageData(imgDataOutput, 0, 0);
-  }
-
-  shouldComponentUpdate(nextProps) {
-    return this.props.match !== nextProps.match;
-  }
-
-  componentDidMount() {
-    const { target, match, options } = this.props;
-    generateDiffData(target, match).then(imgData =>
-      this.diffPixels({ ...imgData, ...{ options } })
-    );
-  }
-
-  componentDidUpdate() {
-    const { target, match, options } = this.props;
-    generateDiffData(target, match).then(imgData =>
-      this.diffPixels({ ...imgData, ...{ options } })
-    );
-  }
-
-  render() {
-    return (
-      <canvas
-        className="diff-output"
-        ref={this.canvas}
-        width="100%"
-        height="0"
-      ></canvas>
-    );
-  }
-}
 export default DiffOutput;
 
 DiffOutput.propTypes = {
-  onDiffResult: PropTypes.func,
-  target: PropTypes.string,
-  match: PropTypes.string,
-  options: PropTypes.shape({
-    threshold: PropTypes.number,
-    includeAA: PropTypes.bool,
-    alpha: PropTypes.number,
-    aaColor: PropTypes.arrayOf(PropTypes.number),
-    diffColor: PropTypes.arrayOf(PropTypes.number)
-  })
+  snapshot: PropTypes.string
 };
